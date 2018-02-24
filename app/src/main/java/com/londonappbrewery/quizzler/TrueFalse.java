7512f26c29da207a93ee1cfe4ba4b07751a75da2
chiranjev <shl12321@rediffmail.com>
2018-02-24 21:11:49 +0530

    quiz app

---
+++ app/src/main/java/com/londonappbrewery/quizzler/TrueFalse.java
@@ -0,0 +1,33 @@
+package com.londonappbrewery.quizzler;
+
+/**
+ * Created by Chiranjev Koul on 22-02-2018.
+ */
+
+public class TrueFalse
+{
+    private int mQuestionID;
+    private boolean mAnswer;
+
+    public TrueFalse(int questionResourceID,boolean trueOrFalse)
+    {
+        mQuestionID=questionResourceID;
+        mAnswer=trueOrFalse;
+    }
+
+    public int getQuestionID() {
+        return mQuestionID;
+    }
+
+    public void setQuestionID(int questionID) {
+        mQuestionID = questionID;
+    }
+
+    public boolean isAnswer() {
+        return mAnswer;
+    }
+
+    public void setAnswer(boolean answer) {
+        mAnswer = answer;
+    }
+}
